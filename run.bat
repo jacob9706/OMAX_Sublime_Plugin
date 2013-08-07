4d450f0da5904541cbc9bacae49f471635d49a6c
Jacob Ebey <jacob.o.ebey@gmail.com>
2013-08-07 11:47:03 -0700

    Adding file

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @cd %programfiles(x86)%\
 
-@if %ERRORLEVEL% == 0 (echo x64&&"C:\Program Files (x86)\OMAX Corporation\OMAX_Layout_and_Make\Layout.exe" %1)
+@if %ERRORLEVEL% == 0 (echo x64&&start /d "C:\Program Files (x86)\OMAX Corporation\OMAX_Layout_and_Make" Layout.exe %1)
 
-@if %ERRORLEVEL% == 1 (echo x86&&"C:\Program Files\OMAX Corporation\OMAX_Layout_and_Make\Layout.exe %1")
+@if %ERRORLEVEL% == 1 (echo x86&&start /d "C:\Program Files\OMAX Corporation\OMAX_Layout_and_Make" Layout.exe %1)
 
