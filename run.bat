302d53cb7259eade50c736b4df5cc2ea753e56d5
Jacob Ebey <jacob.o.ebey@gmail.com>
2013-08-21 15:03:50 -0700

    Updated build script to run as a standalone.

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @cd %programfiles(x86)%\
 
-@if %ERRORLEVEL% == 0 (start /d "C:\Program Files (x86)\OMAX Corporation\OMAX_Layout_and_Make" OMAX_Script.exe /AutoRun %1)
-@if %ERRORLEVEL% == 1 (start /d "C:\Program Files\OMAX Corporation\OMAX_Layout_and_Make" OMAX_Script.exe /AutoRun %1)
+@if %ERRORLEVEL% == 0 (start /d "C:\Program Files (x86)\OMAX Corporation\OMAX_Layout_and_Make" Layout.exe %1)
+@if %ERRORLEVEL% == 1 (start /d "C:\Program Files\OMAX Corporation\OMAX_Layout_and_Make" Layout.exe %1)
---
+++ run_standalone.bat
@@ -0,0 +1,4 @@
+@cd %programfiles(x86)%\
+
+@if %ERRORLEVEL% == 0 (start /d "C:\Program Files (x86)\OMAX Corporation\OMAX_Layout_and_Make" OMAX_Script.exe /AutoRun /NoUserInterface %1)
+@if %ERRORLEVEL% == 1 (start /d "C:\Program Files\OMAX Corporation\OMAX_Layout_and_Make" OMAX_Script.exe /AutoRun /NoUserInterface %1)
