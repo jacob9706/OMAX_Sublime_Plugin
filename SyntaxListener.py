e2680f64ce20f257a2a0767c1fa213933ba49d3b
Jacob Ebey <jacob.o.ebey@gmail.com>
2013-08-26 09:16:40 -0700

    The plugin now sets the syntax appropriatly to either PascalScript or C++ Script based on the first line of the file.

---
+++ SyntaxListener.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin
+
+class ExampleCommand(sublime_plugin.EventListener):
+	def on_load(self, view):
+		window = view.window()
+		v = window.active_view()
+		fn = v.file_name()
+
+		with open(fn) as f:
+			line = f.readline()
+			if 'Pascal' in line:
+				view.settings().set('syntax', 'Packages/OMAX_Sublime_Plugin-master/PascalScript/OMAX_Script_Pascal.tmLanguage')
+				print "PascalScript"
+			elif 'C++' in line:
+				view.settings().set('syntax', 'Packages/OMAX_Sublime_Plugin-master/CPPScript/OMAX_C++.tmLanguage')
+				print "C++ Script"
