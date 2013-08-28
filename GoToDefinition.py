dacc96746345c4ab3fcad4c0eb1e76a0fdcf3ef8
Jacob Ebey <jacob.o.ebey@gmail.com>
2013-08-28 11:47:09 -0700

    fixing decoding issue

--- GoToDefinition.py
+++ GoToDefinition.py
@@ -87,140 +87,183 @@
 
 
 import sublime, sublime_plugin
-import os, fnmatch, re
-import threading
+import subprocess
+import re
+import functools
+import os
 
-class DefinitionsIndex:
-    supported_languages = [
-        {
-            "filematch" : "*.rb",
-            "regexp": "(module|def|class) (self\.\w+|\w+)",
-            "extract": lambda m: m.group(2)[5:] if m.group(2).startswith("self.") else m.group(2)
-        },
-        {
-            "filematch": "*.py",
-            "regexp": "(def|class) (\w+)",
-            "extract": lambda m: m.group(2)
-        },
-        {
-            "filematch": "*.scala",
-            "regexp": "(trait|object|class|def) (\w+)",
-            "extract": lambda m: m.group(2)
-        },
-        {
-            "filematch": "*.js",
-            "regexp": "function (\w+)|(\w+): function",
-            "extract": lambda m: m.group(1) or m.group(2)
-        },
-        {
-        	"filematch": "*.omaxscript",
-        	"regexp": "(function|procedure|void|bool|int|float|double) (\w+)",
-        	"extract": lambda m: m.group(1) or m.group(2)
-        }
-    ]
+#borrowed from Git Plugin by David Lynch
+#https://github.com/kemayo/sublime-text-2-git/
+def do_when(conditional, callback, *args, **kwargs):
+  if conditional():
+      return callback(*args, **kwargs)
+  sublime.set_timeout(functools.partial(do_when, conditional, callback, *args, **kwargs), 50)
+
+#Gets current word and performs a grep on project folders
+#to see if it has a function definition or not
+class GoToFunctionCommand(sublime_plugin.TextCommand):
+  files = []
+  excludedFiles = None
+  excludedDirs = None
+  settings = None
+
+  def run(self, text):
+    self.settings = sublime.load_settings("go2function.sublime-settings")
+    self.excludedDirs = self.settings.get("folder_exclude_patterns")
+    # sublime.message_dialog(str(self.excludedDirs))
+    view = self.view
+
+    #get current word
+    selection_region = view.sel()[0]
+    word_region = view.word(selection_region)
+    word = view.substr(word_region).strip()
+    word = re.sub('[\(\)\{\}\s]', '', word)
+
+    #get folders to search
+    window = sublime.active_window()
+    proj_folders = window.folders()
+
+    if word != "":
+      print "[Go2Function] Searching for 'function "+word+"'..."
+      files = []
+
+      for dir in proj_folders:
+        resp = self.doGrep(word, dir, self.getExcludedDirs(view))
+        if len(resp) > 0:
+          files.append(resp)
+
+      if len(files) == 0:
+        print "[Go2Function] "+word+" not found"
+        sublime.error_message("could not find function definition for "+word)
+      elif len(files) == 1:
+        self.openFileToDefinition(files[0])
+      else:
+        self.files = files
+        paths = []
+
+        for path,line in files:
+          paths.append(path+":"+str(line))
+
+        window.show_quick_panel(paths, lambda i: self.selectFile(i))
+
+  def selectFile(self, index):
+    if index > -1 and len(self.files) > index:
+      self.openFileToDefinition(self.files[index])
+
+  #actually do the search
+  def doGrep(self, word, directory, nodir):
+    out = ()
+    search_terms = self.getSearchTerms(word)
+    # sublime.message_dialog(str(self.excludedDirs))
+    for r,d,f in os.walk(directory):
+      
+      if self.canCheckDir(r, nodir):
+        for files in f:
+          fn = os.path.join(r, files)
+
+          if self.canCheckFile(fn): #a long list of patterns can slow this down...
+            search = open(fn, "r")
+            lines = search.readlines()
+
+            for n, line in enumerate(lines):
+              
+              for find in search_terms:
+                
+                if find in line:
+                  out = (fn, n)
+                  break
+
+            search.close()
+
+    return out
+
+  def getSearchTerms(self, word):
+    wordstr = str(word)
+    definitions = self.settings.get("definitions")
+    lookup = []
+
+    for func in definitions:
+      lookup.append(str(re.sub('\$NAME\$', wordstr, func)))
+
+    return lookup
+
+  def getExcludedDirs(self, view):
+    #this gets the folder_exclude_patterns from the settings file, not the project file
+    if self.excludedDirs:
+      return self.excludedDirs
+    else:
+      dirs = view.settings().get("folder_exclude_patterns", [".git", ".svn", "CVS", ".hg"]) #some defaults
+      self.excludedDirs = dirs
+      return dirs
+
+  def getExcludedFiles(self):
+    if self.excludedFiles:
+      return self.excludedFiles
+    else:
+      #check plugin settings, then global
+      patterns = self.settings.get("file_exclude_patterns", self.view.settings().get("file_exclude_patterns"))
+
+      #no file_exclude rules
+      if not patterns:
+        return None
+
+      #merge patterns into single regex
+      rpatterns = []
+
+      for pattern in patterns:
+        pattern = re.sub('\*\.', '.', pattern)
+        pattern = re.escape(str(pattern))
+        rpatterns.append(pattern)
+
+      combined = "(" + ")|(".join(rpatterns) + ")"
+
+      #store regex
+      self.excludedFiles = combined
+
+      return combined
+
+  def canCheckDir(self, dir, excludes):
+    for exclude in excludes:
+      if exclude in dir:
+        return False
+
+    return True
+
+  def canCheckFile(self, filename):
+    patterns = self.getExcludedFiles()
+
+    if not patterns:
+      return True
+
+    if(re.match(patterns, filename)):
+      return False
+
+    return True
+
+  #open the file and scroll to the definition
+  def openFileToDefinition(self, response):
+    file, line = response
+
+    print "[Go2Function] Opening file "+file+" to line "+str(line)
     
-    def __init__(self):
-        self.definitions_index = []
-        self.status = "uninitialized"
+    window = sublime.active_window()
+    new_view = window.open_file(file)
 
-    def index_file(self, filename):
-        basename = os.path.basename(filename)
-        languages = filter(lambda l: fnmatch.fnmatch(basename, l["filematch"]), DefinitionsIndex.supported_languages)
-        if len(languages) == 1:
-            language = languages[0]
-            f = open(filename)
-            position = 0
-            for line in f:
-                match = re.search(language["regexp"], line)
-                if match:
-                    name = language["extract"](match)
-                    self.definitions_index.append(Definition(name, filename, position))
-                position += len(line)
-            f.close()
+    do_when(
+      lambda: not new_view.is_loading(), 
+      lambda: self.cursorToPos(new_view, line)
+    )
 
-    def reindex_file(self, filename):
-        if self.is_initialized():
-            self.definitions_index = filter(lambda x: x.filename != filename, self.definitions_index)
-            self.index_file(filename)
+  #move cursor to the definition too
+  def cursorToPos(self, view, line):
+    nav_line = line - 1
+    nav_pt = view.text_point(nav_line, 0)
+    fn_line = line
+    pt = view.text_point(fn_line, 0)
 
-    def index_folders(self, folders):
-        for directory in folders:
-            for root, dirs, files in os.walk(directory):
-                for basename in files:
-                    filename = os.path.join(root, basename)
-                    self.index_file(filename)
+    view.set_viewport_position(view.text_to_layout(nav_pt))
 
-    def build(self, folders, callback):
-        print("Start building index...")
-        self.index_folders(folders)
-        self.status = "initialized"
-        print("Building index finished")
-        sublime.set_timeout(lambda: callback(self.definitions_index), 0)
+    view.sel().clear()
+    view.sel().add(sublime.Region(pt))
 
-    def is_initialized(self):
-        return self.status == "initialized"
-
-    def is_loading(self):
-        return self.status == "loading"
-
-    def build_if_needed_and_do(self, callback):
-        if self.is_initialized():
-            callback(self.definitions_index)
-        elif not self.is_loading():
-            sublime.status_message("Building index...")
-            self.status = "loading"
-            thread = threading.Thread(target=self.build,args=(sublime.active_window().folders(), callback, ))
-            thread.start()
-
-class Definition:
-    def __init__(self, name, filename, position):
-        self.name = name
-        self.filename = filename
-        self.position = position
-
-definitions_index_by_window = {}
-
-def get_definitions_index():
-    global definitions_index_by_window
-
-    window_id = sublime.active_window().id()
-    
-    if not window_id in definitions_index_by_window:
-        definitions_index_by_window[window_id] = DefinitionsIndex()
-
-    definitions_index = definitions_index_by_window[window_id]
-    return definitions_index
-
-def goto_definition(definition):
-    opened_view = sublime.active_window().open_file(definition.filename)
-    opened_view.sel().clear()
-    opened_view.sel().add(definition.position)
-    opened_view.show(definition.position)
-
-class GoToDefinitionDialogCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        get_definitions_index().build_if_needed_and_do(self.show_panel)
-
-    def show_panel(self, index):
-        items = map(lambda x: [x.name, x.filename], index)
-        process_selected = lambda i: goto_definition(index[i]) if i != -1 else None
-        self.window.show_quick_panel(items, process_selected) 
-                        
-
-class GoToDefinitionCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        get_definitions_index().build_if_needed_and_do(self.go_to_definition)
-
-    def go_to_definition(self, index):
-        word = self.view.substr(self.view.word(self.view.sel()[0]))
-        found_definitions = filter(lambda definition: definition.name == word, index)
-        if len(found_definitions) == 1:
-            goto_definition(found_definitions[0])
-        elif len(found_definitions) > 1:
-            items = map(lambda x: [x.name, x.filename], found_definitions)
-            process_selected = lambda i: goto_definition(found_definitions[i]) if i != -1 else None
-            self.view.window().show_quick_panel(items, process_selected)
-
-class IndexUpdater(sublime_plugin.EventListener):
-    def on_post_save(self, view):
-        get_definitions_index().reindex_file(view.file_name())
+    view.show(pt)
