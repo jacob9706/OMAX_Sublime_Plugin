6f033662060b961565748777e8a8bb852853541b
Jacob Ebey <jacob.o.ebey@gmail.com>
2013-09-04 10:57:42 -0700

    Added documentation and renamed the Language Listener

--- OmaxHelp.py
+++ OmaxHelp.py
@@ -1,10 +1,11 @@
 import sublime, sublime_plugin, re, os
 
+# This is a TextCommand. It operates on the selected text (in this case) and looks up documentation.
 class OmaxHelpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		view = self.view
 
-		#get current word
+		# Get selected text and convert to lowercase
 		selection_region = view.sel()[0]
 		word_region = view.word(selection_region)
 		search_text = view.substr(word_region).strip()
@@ -12,24 +13,35 @@ class OmaxHelpCommand(sublime_plugin.TextCommand):
 		orig_search_text = search_text
 		search_text = search_text.lower()
 
+		# If what we are looking for does not contain "omax_", show error message
 		if search_text == '' or 'omax_' not in search_text:
-			sublime.error_message('"' + search_text + '" Was not found in the help file.')
+			sublime.error_message('"' + search_text + '" is not an OMAX function.')
 			return
 
+		# Get the help file path
 		help_file = sublime.packages_path() + '/OMAX_Sublime_Plugin-master/Functions.omaxscript'
 
+		# Open the help file
 		with open(help_file) as f:
 			lines = f.readlines()
+			# Loop thorugh each line
 			for l in lines:
+				# Convert line to lowercase
 				l = l.lower()
+				# Split the line up at comment
 				tmp_split = l.split('//', 1)
 				if len(tmp_split) < 2:
 						continue
+				# If we meet the format conditions show the documentation
 				if ' ' + search_text + '(' in tmp_split[0] or ' ' + search_text + ';' in tmp_split[0] or  search_text + ':' in tmp_split[0]:
 					sublime.message_dialog('"' + orig_search_text + '":'+ tmp_split[1])
 					return
+		 # If we reach here we did not find anyting so show error message.
 		sublime.error_message('"' + orig_search_text + '" Was not found in the help file.')
 
+
+# This is a text command that will launch the omax help document in
+# the default system viewer for ".rtf" files.
 class OmaxLaunchHelpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		os.system(r'start %APPDATA%\Sublime_Help_Document.rtf')
--- SyntaxListener.py
+++ SyntaxListener.py
@@ -1,11 +1,17 @@
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.EventListener):
+# Create a event listener that on file load checks the first line
+# of a file and sets the syntax accordingly
+class SyntaxSelectorCommand(sublime_plugin.EventListener):
 	def on_load(self, view):
+		# Get the file name
 		fn = view.file_name()
 
-		with open(fn) as f:
+		# Open the file. Once the block finishes the file is closed automatically.
+		with open(fn, 'r') as f:
+			# Get the first line
 			line = f.readline()
+			# Check and set syntax
 			if 'Pascal' in line:
 				view.settings().set('syntax', 'Packages/OMAX_Sublime_Plugin-master/PascalScript/OMAX_Script_Pascal.tmLanguage')
 				print "PascalScript"
